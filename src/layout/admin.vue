23c8a95cc26188158b29f596ce021525bb3cbcb9
Yforever <y_coder@outlook.com>
2024-10-20 10:45:23 +0800

    首页布局搭建

---
+++ src/layout/admin.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container>
+    <el-header>
+      <f-header />
+    </el-header>
+    <el-container>
+      <el-aside>
+        <f-menu></f-menu>
+      </el-aside>
+      <el-main>
+        <f-tag-list />
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup>
+import FHeader from "~/layout/components/FHeader.vue";
+import FMenu from "~/layout/components/FMenu.vue";
+import FTagList from "~/layout/components/FTagList.vue";
+</script>
---
+++ src/layout/components/FMenu.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>菜单导航</div>
+</template>
